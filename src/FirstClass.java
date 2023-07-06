b05e6b50cb6d731bfcf66597384370a7a84784fd
suryajude <jsurya1230@gmail.com>
2023-07-06 09:29:08 +0530

    second commit

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -3,7 +3,23 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("Helo World");
 
 	}
 
 }
+// variables and data types
+
+// variables => container or box
+// to store a data 
+
+/*
+
+int
+float
+double
+bool
+String
+long
+*/
